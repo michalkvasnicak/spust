c3b5292474c0ddc05eea53e856bd1759cbcde6f1
Michal Kvasničák <michal.kvasnicak@gmail.com>
2017-06-07 15:24:08 +0200

    📝 include assets path in server fixture

--- fixtures/test/server/index.js
+++ fixtures/test/server/index.js
@@ -1,9 +1,34 @@
 import { createServer } from 'http';
+import { readFileSync } from 'fs';
+
+let assets = {
+  main: {
+    css: null,
+    js: null,
+  },
+};
 
 const server = createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('ok');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  try {
+    // do not use require because webpack can't figure out expression
+    assets = JSON.parse(readFileSync(process.env.ASSETS_JSON_PATH, { encoding: 'utf8' }));
+  } catch (e) {
+    console.error(e);
+  }
+
+  res.end(`
+    <html>
+      <head>
+        <title>Spust!!</title>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+        <script src="${assets.main.js}"></script>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(3001, '0.0.0.0', () => console.log('listening on 3001'));
