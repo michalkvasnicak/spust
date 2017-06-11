6e2dad84d1dd35d273fe794d35c468ca0fe734f4
Michal Kvasničák <michal.kvasnicak@gmail.com>
2017-06-11 11:16:11 +0200

    🔧 load .env variables for given environment first

--- src/helpers/loadEnvVariables.js
+++ src/helpers/loadEnvVariables.js
@@ -4,9 +4,11 @@ import dotenv from 'dotenv';
 import { readFile } from 'mz/fs';
 import { resolve as resolvePath } from 'path';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 export default async function loadEnvVariables(
   workDir: string,
-  envFileName?: Array<string> = ['.env', '.env.local'],
+  envFileName?: Array<string> = [`.env.${NODE_ENV}`, '.env', '.env.local'],
 ): Promise<{ [key: string]: any }> {
   const envFilesContents: Array<Object> = await Promise.all(
     envFileName.map(filename =>
